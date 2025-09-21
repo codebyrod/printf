17f6b4f9048339c486d4e0df15fe17f2e0b25516
Rodrigo Sousa <rosousa-@student.42sp.org.br>
2025-09-20 23:30:46 -0300

    adjusts to the norm

--- ft_printf.h
+++ ft_printf.h
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rosousa- <rosousa-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 22:28:55 by rosousa-          #+#    #+#             */
-/*   Updated: 2025/09/19 19:48:01 by rosousa-         ###   ########.fr       */
+/*   Updated: 2025/09/20 23:28:36 by rosousa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include "libft/ft_libft.h"
-#include <stdarg.h>
+# include "libft/ft_libft.h"
+# include <stdarg.h>
 
 typedef struct s_index
 {
-	int i;
-	int count;
+	int	i;
+	int	count;
 }	t_index;
 
 int	ft_printf(const char *format, ...);
@@ -28,9 +28,8 @@ int	ft_putchar(int n);
 int	ft_putstr(char *str);
 int	ft_putnbr(int n);
 int	ft_putnbr_uns(unsigned int n);
-int ft_hexadecimal(unsigned int hx, const char str);
+int	ft_hexadecimal(unsigned int hx, const char str);
 int	ft_pointer(unsigned long p);
 int	ft_handle_pointer(unsigned long p);
 
 #endif
-
